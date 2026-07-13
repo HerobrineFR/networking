2507c1e3413108acd4e32faf669f321bd4799aaa
Yazor <yazor@herobrine.fr>
2026-07-13 18:48:32 -0400

    doc: Add AbstractPacket documentation

--- src/main/kotlin/network/AbstractPacket.kt
+++ src/main/kotlin/network/AbstractPacket.kt
@@ -2,10 +2,19 @@ package fr.herobrine.network
 
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload
 
+/**
+ * Type de packet custom.
+ *
+ * Les packets ont une logique interne très légère, puisque ceux-ci sont transmis
+ * par des implémentations de couches réseau.
+ */
 abstract class AbstractPacket: CustomPacketPayload {
     override fun type(): CustomPacketPayload.Type<out CustomPacketPayload> {
         return CustomPacketPayload.Type(packetInfo().identifier)
     }
 
-    abstract protected fun packetInfo(): PacketInfo<*>
+    /**
+     * Renvoie les informations du packet.
+     */
+    protected abstract fun packetInfo(): PacketInfo<*>
 }
