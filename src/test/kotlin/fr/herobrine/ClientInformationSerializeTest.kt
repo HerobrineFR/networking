56c220a73acbd5892981d7b66133ab3d404faebc
Yazor <yazor@herobrine.fr>
2026-07-14 12:34:44 -0400

    chore: Create ClientInfo serialize test

---
+++ src/test/kotlin/fr/herobrine/ClientInformationSerializeTest.kt
@@ -0,0 +1,30 @@
+package fr.herobrine
+
+import fr.herobrine.network.client.ClientInformation
+import fr.herobrine.network.client.ClientProperty
+import fr.herobrine.network.client.meta.ModMetadata
+import net.minecraft.nbt.NbtOps
+import kotlin.test.Test
+import kotlin.test.assertTrue
+import kotlin.test.fail
+
+class ClientInformationSerializeTest {
+    @Test
+    fun `verify clientinfo serialization`() {
+        val info = ClientInformation()
+
+        info.setProperty(ClientProperty.MOD_METADATA, ModMetadata(
+            usesHerobrineTweaks = true
+        ))
+
+        ClientInformation.CODEC.encodeStart(NbtOps.INSTANCE, info).ifSuccess { tag ->
+            ClientInformation.CODEC.decode(NbtOps.INSTANCE, tag).ifSuccess { pair ->
+                assertTrue { pair.first.getProperty(ClientProperty.MOD_METADATA).usesHerobrineTweaks }
+            }
+
+            fail("ClientInfo tag decode failed")
+        }
+
+        fail("ClientInfo encoding to NBT failed")
+    }
+}
