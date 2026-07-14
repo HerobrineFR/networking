8c99be92778208dc97dfbcc7dd8ab64780900632
Yazor <yazor@herobrine.fr>
2026-07-14 15:41:32 -0400

    feat: Add update skin packet

---
+++ src/main/kotlin/network/profile/ServerboundUpdateSkinPacket.kt
@@ -0,0 +1,31 @@
+package fr.herobrine.network.profile
+
+import fr.herobrine.network.AbstractPacket
+import fr.herobrine.network.PacketInfo
+import fr.herobrine.util.identifier
+import net.minecraft.network.codec.StreamCodec
+import net.minecraft.world.entity.player.PlayerSkin
+
+/**
+ * Packet envoyé au serveur, permettant d'appliquer un changement de skin. (Non implémenté)
+ *
+ * @property skinPatch Nouvelles données du skin
+ */
+data class ServerboundUpdateSkinPacket(
+    val skinPatch: PlayerSkin.Patch
+): AbstractPacket() {
+    override fun packetInfo(): PacketInfo<out AbstractPacket> {
+        return PACKET_INFO
+    }
+
+    companion object {
+        @JvmField
+        val PACKET_INFO = PacketInfo(
+            identifier("herobrine:update_skin"),
+            streamCodec = StreamCodec.composite(
+                PlayerSkin.Patch.STREAM_CODEC, ServerboundUpdateSkinPacket::skinPatch,
+                ::ServerboundUpdateSkinPacket
+            )
+        )
+    }
+}
